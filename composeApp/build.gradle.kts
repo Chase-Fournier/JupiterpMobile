0ee257fd6ca7e67e6e9e7a1da497466c726ca639
cfournier <fournier.chase2@gmail.com>
2026-06-15 17:33:01 -0400

    small bump

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -70,8 +70,8 @@ android {
         applicationId = "com.jupiterp.jupiterpmobile"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.4"
     }
     packaging {
         resources {
